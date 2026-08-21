cf3abd31d7c98d89f3a35c56d88ed21a0b6d471a
Hayami Shuhei <mail.hayami@gmail.com>
2026-08-22 07:33:39 +0900

    test(gpu): cover eviction key attribution

--- test/resource_cache_metrics_test.dart
+++ test/resource_cache_metrics_test.dart
@@ -2,12 +2,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:maplibre_flutter_gpu/src/gpu/resource_cache.dart';
 
 void main() {
-  test('expiry eviction callback reports removed values', () {
+  test('expiry eviction callback reports removed keys and values', () {
     final cache = <({int id, int version}), ({int lastUsed, int bytes})>{
       (id: 1, version: 1): (lastUsed: 1, bytes: 100),
       (id: 1, version: 2): (lastUsed: 10, bytes: 200),
     };
     var evictedBytes = 0;
+    ({int id, int version})? evictedKey;
 
     evictExpiredCacheVersions(
       cache,
@@ -16,11 +17,15 @@ void main() {
       versionOf: (key) => key.version,
       lastUsedOf: (value) => value.lastUsed,
       unusedRetentionFramesOf: (_) => 100,
-      onEvict: (value) => evictedBytes += value.bytes,
+      onEvict: (key, value) {
+        evictedKey = key;
+        evictedBytes += value.bytes;
+      },
     );
 
     expect(cache.keys, contains((id: 1, version: 2)));
     expect(cache.keys, isNot(contains((id: 1, version: 1))));
+    expect(evictedKey, (id: 1, version: 1));
     expect(evictedBytes, 100);
   });
 }
